f70325c4db44f536ae006e85cee2d8f7d8679bc0
jindalakhil <akhiljindal222@gmail.com>
2020-09-28 22:43:21 +0530

    [Akhil]Added: Master initial commit

---
+++ src/com/capg/pincode/pinCode.java
@@ -0,0 +1,9 @@
+package com.capg.pincode;
+
+import java.util.regex.*;
+
+public class pinCode {
+	public static void main(String[] args) {
+		System.out.println("Welcome to pincode validation");
+	}
+}
